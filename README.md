022a40578ec1b7870f70dd912ba281c85eca04ea
enzomnlt <enzomnlt@gmail.com>
2024-11-26 17:16:39 +0100

    Màj README et début main

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
 # Deux-salles-deux-ambiances
 
-# Compilation :
+## Compilation :
 
-# Mode d'emploi :
+Pour lancer ce projet, il faut dans un premier temps se placer dans le dossier `build` :
+```
+cd build
+```
+Il faut ensuite le compiler en faisant dans l'ordre:
+```
+cmake ..
+make
+```
+L'éxécutable est alors crée dans le dossier `bin`. On peut alors le lancer avec la commande :
+```
+../bin/DSDA
+```
+
+## Mode d'emploi :
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,33 @@
 #include <GLFW/glfw3.h>
 
+int window_width  = 800;
+int window_height = 800;
+
 int main()
 {
     /* Initialize the library */
     if (!glfwInit()) {
         return -1;
     }
+
+    GLFWwindow* window = glfwCreateWindow(window_width, window_height, "Deux salles, deux ambiances", nullptr, nullptr);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window)) {
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
 
     return 0;
 }
